9c276f48ad0ddf74b980217a37d86dcce7591e20
tauri_s <tauristclaire@gmail.com>
2023-09-25 16:08:32 -0700

    Arrays have literal syntax that allows for new keyword not to be used

--- js/new-this.js
+++ js/new-this.js
@@ -6,3 +6,12 @@ console.log(myDate.getFullYear());
 
 console.log(typeof myDate); //object
 console.log(myDate instanceof Date); //true
+
+/* 
+* Same with arrays, but we don't have to use the new keyword
+* Arrays allow for 'literal syntax' which means the new keyword isn't necessary
+* Date objects do not have literal syntax
+*/
+const names = [`tauri`, `danielle`];
+console.log(typeof names);
+console.log(names instanceof Array);
