99d85a9d1f8060593b878f5d3dd54be6a19a40d2
tauri_s <tauristclaire@gmail.com>
2023-09-25 16:16:33 -0700

    The basics of classes and extending

--- js/new-this.js
+++ js/new-this.js
@@ -22,3 +22,16 @@ console.log(names instanceof Array);
 const span = document.createElement(`span`);
 console.log(span); //<span></span>
 console.log(span instanceof Element); //true
+console.log(span.constructor);
+console.dir(span.constructor);
+
+console.log(span instanceof HTMLSpanElement);
+console.log(span instanceof Node);
+//Classes and extending
+/*
+* things can start very basic like a node with text. 
+* Then it can go a little further and become an element, and have a tag and attributes. 
+* And then it can go even further and become a special kind of element like an image or div. 
+* In all of those cases, the element inherits the Node 
+* and the HTMLSpanElement inherits the Element.
+*/
